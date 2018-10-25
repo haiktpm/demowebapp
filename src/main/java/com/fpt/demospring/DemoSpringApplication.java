ce2746cac6cd26251a79ac6275fddb9eea025eb9
Truyen <truyenhv@fsoft.com.vn>
2018-10-25 17:29:12 +0700

    support war build

--- src/main/java/com/fpt/demospring/DemoSpringApplication.java
+++ src/main/java/com/fpt/demospring/DemoSpringApplication.java
@@ -1,13 +1,24 @@
 package com.fpt.demospring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringApplication {
+public class DemoSpringApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return configureApplication(application);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
 	}
 
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(DemoSpringApplication.class).bannerMode(Banner.Mode.OFF);
+    }
 }
